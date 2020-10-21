a617cfa6a2b00ba2401e0474a71762cddc837925
서지원 <wldnjs7825@naver.com>
2020-10-21 12:22:42 +0900

    ADD 백준 브론즈3 별찍기1

---
+++ 백준/src/브론즈3/별찍기1.java
@@ -0,0 +1,20 @@
+package 브론즈3;
+
+import java.util.Scanner;
+
+public class 별찍기1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
