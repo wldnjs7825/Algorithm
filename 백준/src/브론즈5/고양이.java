bf11c378b2deab9995586942e7295e5d5015b110
서지원 <wldnjs7825@naver.com>
2020-10-21 12:37:44 +0900

    ADD 백준 브론즈5 고양이

---
+++ 백준/src/브론즈5/고양이.java
@@ -0,0 +1,15 @@
+package 브론즈5;
+
+public class 고양이 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\\    /\\\n");
+		sb.append(" )  ( ')\n");
+		sb.append("(  /  )\n");
+		sb.append(" \\(__)|\n");
+		System.out.println(sb);
+		
+	}
+
+}
