d66b1ee805a986d5b243cd2197c403c75f248139
Seo Jiwon <wldnjs7825@naver.com>
2021-05-29 00:50:47 +0900

    ADD 실버3 N과M(5)

---
+++ 백준/src/실버3/N과M5.java
@@ -0,0 +1,50 @@
+package ½Ç¹ö3;
+
+import java.util.*;
+
+public class N°úM5 {
+	static int N,M;
+	static int[] num,result;
+	static boolean[] visited;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		num = new int[N];
+		result = new int[M];
+		visited = new boolean[N];
+		sb = new StringBuilder();
+			
+		for(int i=0; i<N; i++) {
+			num[i] = sc.nextInt();			
+		}
+		
+		Arrays.sort(num);
+		
+		Perm(0);
+		
+		System.out.println(sb);
+	}
+	
+	private static void Perm(int idx) {
+		if(idx == M) {
+			for(int i=0; i<M; i++) {
+				sb.append(result[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=0; i<N; i++) {		
+			if(!visited[i]) {
+				visited[i] = true;
+				result[idx] = num[i];
+				Perm(idx+1);
+				visited[i] = false;
+			}
+		}
+	}
+}
