dcbb23641b1d70b0330bc886e813520b7d8cb094
Seo Jiwon <wldnjs7825@naver.com>
2022-03-01 22:49:25 +0900

    ADD 백준 브론즈5 ??!

---
+++ 백준/src/브론즈5/물음표물음표느낌표.java
@@ -0,0 +1,13 @@
+package 브론즈5;
+
+import java.util.*;
+
+public class 물음표물음표느낌표 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str;
+		str = sc.next();
+		
+		System.out.print(str+"??!");
+	}
+}
