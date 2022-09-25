b20e9fe638a6705f554822e8f8d93dc3f9130e47
Seo Jiwon <wldnjs7825@naver.com>
2022-09-25 23:09:02 +0900

    ADD 브론즈5 별찍기1

---
+++ 백준/src/브론즈5/별찍기1.java
@@ -0,0 +1,19 @@
+package 브론즈5;
+import java.util.*;
+
+public class 별찍기1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=0; j<i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
