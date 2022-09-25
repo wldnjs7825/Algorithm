9d0952e74fe2b536a6452fe655578f6416472c7b
Seo Jiwon <wldnjs7825@naver.com>
2022-09-25 23:24:38 +0900

    ADD 브론즈4 별찍기2

---
+++ 백준/src/브론즈4/별찍기2.java
@@ -0,0 +1,26 @@
+package 브론즈4;
+
+import java.util.*;
+
+public class 별찍기2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=N-i; j>0; j--) {
+				sb.append(" ");
+			}
+			
+			for(int j=0; j<i; j++) {
+				sb.append("*");
+			}
+			
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
