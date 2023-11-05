4a30d549c01c41624fbed4a95b580ea87a489422
wldnjs7825@naver.com <wldnjs7825@naver.com>
2023-11-05 18:41:22 +0900

    ADD Bronze4 PrintStar_202311

---
+++ 백준/src/Bronze4/PrintStar2_202311.java
@@ -0,0 +1,25 @@
+package Bronze4;
+
+import java.util.*;
+
+public class PrintStar2_202311 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N-i-1; j++) {
+				sb.append(' ');
+			}
+			for(int j=0; j<=i; j++) {
+				sb.append('*');
+			}
+			sb.append('\n');
+		}
+
+		System.out.print(sb);
+	}
+}
